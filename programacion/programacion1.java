1cf684e716decc60720dca31bfee4c43ab9d81eb
miguel <miguel.martinvilar@educa.madrid.org>
2023-10-30 10:19:53 +0100

    entornos

---
+++ programacion/programacion1.java
@@ -0,0 +1,17 @@
+/*
+Generar enteros de 3 en 3 comenzando por 2 hasta el valor máximo menor que 30 y calcular
+la suma de los números generados que sean divisibles por 5.
+ */
+package ejercicios_basicos_de_programacion;
+
+public class Ejercicio23 {
+    public static void main(String[] args) {
+        int suma=0;
+        for (int i = 2; i < 30; i=i+3) {
+            
+            if(i%5==0)
+                suma=suma+i;
+        }
+        System.out.println("La suma de los números generados que sean divisibles por 5 es "+suma);
+    }
+}
